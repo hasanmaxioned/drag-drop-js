47d1921e237251e342f7f7f7d6cffb45311c37f7
hasanmaxioned <hasanm@axioned.com>
2022-12-14 16:07:31 +0530

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,86 @@
-const dragSpan = document.querySelectorAll(".drag-box");
-const dropArea = document.querySelectorAll(".box-layout");
-const dragStart = document.querySelector(".start");
-const dragStop = document.querySelector(".end");
+const dragBox = document.querySelectorAll(".drag-box");
+const dropHere = document.querySelectorAll(".drop-here");
+const startEvent = document.querySelector(".start");
+let restartEvent = document.querySelector(".restart");
 
-function startEvent() {
-  dragStart.addEventListener("click", () => {
-    dragSpan.forEach ( start => {
-    let isEvent = start.setAttribute("draggable", "true");
-    });
-  });
-}
-startEvent();
+startEvent.addEventListener("click", function () {
+  const initialText = "start";
 
-function endEvent() {
-  dragStop.addEventListener("click", () => {
-    dragSpan.forEach( end => {
-      let isEventOn = end.setAttribute("draggable", "false");
-    });
-  });
-};
-endEvent();
-
-
-
-// let elements = document.querySelectorAll("span");
-
-// for (let i = 0, element; (element = elements[i]); i++) {
-//   console.log(element);
-// }
-
-dropArea.forEach((element) => {
-  element.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    // console.log("dragover event is fire");
-  });
-
-  element.addEventListener("drop", (event) => {
-    dragSpan.forEach((start) => {
-      start.addEventListener("click", (e) => {
-                event.target.append(dragSpan);
-                console.log(e);
-
-      })
-      // let isEvent = start.getAttribute("draggable");
-      // if (isEvent == "true") {
-      //   event.target.append(start[i]);
-      // }
-    });
-
-    // console.log(dragSpan);
-
-    // console.log("drop event is fire");
-  });
+  if (startEvent.textContent == initialText) {
+    startEvent.textContent = "stop";
+    start();
+  } else {
+    startEvent.textContent = initialText;
+    stop();
+  }
 });
 
-// function valueFind() {
-//   let elements = document.querySelectorAll("span");
-//   for (let i = 0, element; (element = elements[i]); i++) {
-//     console.log(element);
-//   }
-// }
+function start() {
+  dragBox.forEach((element) => {
+    let isEvent = element.setAttribute("draggable", "true");
+  });
+}
+
+function stop() {
+  dragBox.forEach((end) => {
+    let isEventOn = end.setAttribute("draggable", "false");
+  });
+}
+
+function restart() {
+  restartEvent.addEventListener("click", function () {
+    window.location.reload();
+  });
+}
+restart();
+
+let draggedItem;
+for (let i = 0; i < dragBox.length; i++) {
+  const item = dragBox[i];
+
+  item.addEventListener("dragstart", function () {
+    console.log("dragStart");
+    draggedItem = item;
+    setTimeout(function () {
+      item.style.display = "none";
+    }, 0);
+  });
+
+  item.addEventListener("dragend", function () {
+    console.log("drag end");
+    setTimeout(function () {
+      draggedItem.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
+
+  for (let j = 0; j < dropHere.length; j++) {
+    const list = dropHere[j];
+
+    list.addEventListener("dragover", function (e) {
+      console.log("drag box over");
+      e.preventDefault();
+    });
+
+    list.addEventListener("dragenter", function (e) {
+      console.log("drag box enter");
+      e.preventDefault();
+      list.style.backgroundColor = "rgb(149 145 145 / 24%)";
+      list.style.borderRadius = "10px";
+    });
+
+    list.addEventListener("dragleave", function (e) {
+      console.log("box leave");
+      e.preventDefault();
+      list.style.backgroundColor = "rgb(223 215 215 / 24%)";
+      list.style.borderRadius = "10px";
+    });
+
+    list.addEventListener("drop", function (e) {
+      console.log("drop func run");
+      list.append(draggedItem);
+      list.style.backgroundColor = "#fff";
+      list.style.borderRadius = "10px";
+    });
+  }
+}
