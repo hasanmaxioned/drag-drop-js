f6841ff0b8b6097909277544ea53ad22ea92e78c
hasanmaxioned <hasanm@axioned.com>
2022-12-20 19:26:40 +0530

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const dropHere = document.querySelectorAll(".drop-here");
 const startEvent = document.querySelector(".start");
 let restartEvent = document.querySelector(".restart");
 
+// console.log(dragBox);
 startEvent.addEventListener("click", function () {
   const initialText = "start";
 
@@ -16,13 +17,14 @@ startEvent.addEventListener("click", function () {
 });
 
 function start() {
-  dragBox.forEach((element) => {
-    let isEvent = element.setAttribute("draggable", "true");
+  dragBox.forEach((start) => {
+    let isEvent = start.setAttribute("draggable", "true");
   });
 }
 
 function stop() {
   dragBox.forEach((end) => {
+    // console.log(end);
     let isEventOn = end.setAttribute("draggable", "false");
   });
 }
@@ -39,48 +41,74 @@ for (let i = 0; i < dragBox.length; i++) {
   const item = dragBox[i];
 
   item.addEventListener("dragstart", function () {
-    console.log("dragStart");
+    // console.log("dragStart");
     draggedItem = item;
     setTimeout(function () {
       item.style.display = "none";
-    }, 0);
+      // console.log("hi item",item);
+
+    });
   });
 
   item.addEventListener("dragend", function () {
-    console.log("drag end");
+    // console.log("drag end");
     setTimeout(function () {
       draggedItem.style.display = "block";
-      draggedItem = null;
-    }, 0);
+      // console.log("hi",draggedItem);
+    });
   });
 
-  for (let j = 0; j < dropHere.length; j++) {
+}
+for (let j = 0; j < dropHere.length; j++) {
     const list = dropHere[j];
-
+    // console.log("im a deop",list);
     list.addEventListener("dragover", function (e) {
-      console.log("drag box over");
+      // console.log("drag box over");
       e.preventDefault();
-    });
-
-    list.addEventListener("dragenter", function (e) {
-      console.log("drag box enter");
-      e.preventDefault();
-      list.style.backgroundColor = "rgb(149 145 145 / 24%)";
+      list.style.backgroundColor = "rgb(133 131 130)";
       list.style.borderRadius = "10px";
     });
 
-    list.addEventListener("dragleave", function (e) {
-      console.log("box leave");
-      e.preventDefault();
-      list.style.backgroundColor = "rgb(223 215 215 / 24%)";
+    list.addEventListener("dragenter", function () {
+      // console.log("drag box enter");
+      list.style.backgroundColor = "rgb(229 29 29 / 24%)";
+      list.style.borderRadius = "10px";
+    });
+
+    list.addEventListener("dragleave", function () {
+      // console.log("box leave");
+      list.style.backgroundColor = "rgb(255 239 122 / 63%)";
       list.style.borderRadius = "10px";
     });
 
     list.addEventListener("drop", function (e) {
-      console.log("drop func run");
+      // console.log("drop func run");
+      dragBox.forEach(function (element, idx) {
+        let valueOfBox = element.getAttribute('data-value');
+        console.log(valueOfBox);
+      })
+      dropHere.forEach (function (drEl) {
+        if (drEl.getElementsByClassName("drag-box")) {
+          console.log("hii");
+        }
+      })
       list.append(draggedItem);
-      list.style.backgroundColor = "#fff";
+      // console.log(draggedItem);
+      
+      list.style.backgroundColor = "rgb(219 184 154)";
       list.style.borderRadius = "10px";
+      console.log(draggedItem);
     });
+
   }
-}
+
+
+  // asynchronus function 
+// let a = 10;
+// console.log(a);
+
+// setTimeout(() => {
+//  console.log("hi hasan"); 
+// });
+
+// console.log("hrllo ", a);
